e2908042bfddb79f8b4c3f30f8f65b76ccb9f8ea
Remson <genevievejoshua@gmail.com>
2015-02-26 20:24:44 +0800

    more

---
+++ app/src/main/java/iacjeep/com/serjeep/Jeep.java
@@ -0,0 +1,36 @@
+package iacjeep.com.serjeep;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.Activity;
+
+public class Jeep extends Activity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    public void onClick_getJeepRoute(View view){
+        Intent getIntent = new Intent(this,mapJeep.class);
+        final int result = 1;
+        startActivity(getIntent);
+    }
+
+
+}
